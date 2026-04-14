77d839cc52de4f6d506705a1cb6ff644930445f7
Vu Xuan Sang <114934081+104180388@users.noreply.github.com>
2026-04-14 15:32:36 +0700

    QuadraticEquation

--- Module2/OOP/src/Main.java
+++ Module2/OOP/src/Main.java
@@ -4,13 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the width:");
-        double width = scanner.nextDouble();
-        System.out.print("Enter the height:");
-        double height = scanner.nextDouble();
-        Rectangle rectangle = new Rectangle(width, height);
-        System.out.println("Your Rectangle \n"+ rectangle.display());
-        System.out.println("Perimeter of the Rectangle: "+ rectangle.getPerimeter());
-        System.out.println("Area of the Rectangle: "+ rectangle.getArea());
+        System.out.print("Enter the a:");
+        double a  = scanner.nextDouble();
+        System.out.print("Enter the b:");
+        double b  = scanner.nextDouble();
+        System.out.print("Enter the c:");
+        double c  = scanner.nextDouble();
+
+        QuadraticEquation demo = new QuadraticEquation(a, b, c);
+        if(demo.delta > 0){
+            System.out.println("The equation has 2 root: "+demo.getRoot1()+" and "+demo.getRoot2());
+        }
+        else if(demo.delta == 0){
+            System.out.println("The equation has 1 root: "+demo.getRoot1());
+        }
+        else{
+            System.out.println("The equation has no real root");
+        }
     }
 }
