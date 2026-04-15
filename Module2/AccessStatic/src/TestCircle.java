6f39fdb5962fc166f686961433af1d8502697bea
Vu Xuan Sang <114934081+104180388@users.noreply.github.com>
2026-04-15 16:01:31 +0700

    circle

---
+++ Module2/AccessStatic/src/TestCircle.java
@@ -0,0 +1,9 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle c = new Circle();
+        c.getArea();
+        c.getRadius();
+        System.out.println("Area is: " + c.getArea());
+        System.out.println("Radius is: " + c.getRadius());
+    }
+}
