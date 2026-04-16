4f6f416c0322d190424076e141aedfc5fb5e7911
Vu Xuan Sang <114934081+104180388@users.noreply.github.com>
2026-04-16 15:52:14 +0700

    shape

--- Module2/AccessStatic/src/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        Student2 student2 = new Student2();
-
-        System.out.println(student2.getName());
-        System.out.println(student2.getClasses());
-    }
-}
