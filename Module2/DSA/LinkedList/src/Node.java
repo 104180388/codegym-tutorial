83914beb7fc287608d3b1321717d311a7f23e914
Vu Xuan Sang <114934081+104180388@users.noreply.github.com>
2026-04-24 17:20:53 +0700

    my linked list

---
+++ Module2/DSA/LinkedList/src/Node.java
@@ -0,0 +1,13 @@
+class Node {
+    Node next;
+    Object data;
+
+    public Node(Object data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public Object getData() {
+        return data;
+    }
+}
