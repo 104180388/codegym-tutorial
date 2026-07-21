618e086e0703fff3fc4cec8a228ad691b5a8e3b3
Vu Xuan Sang <114934081+104180388@users.noreply.github.com>
2026-07-21 14:53:32 +0700

    name

---
+++ Module4/5-music-player/src/main/java/org/example/_musicplayer/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.example._musicplayer;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
