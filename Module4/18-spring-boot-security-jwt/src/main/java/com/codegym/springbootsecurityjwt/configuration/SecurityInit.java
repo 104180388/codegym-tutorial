ee3670b83154daeb78addca4170bca771d34eed1
admin <sangvuxuan6784@gmail.com>
2026-08-21 17:05:41 +0700

    18

---
+++ Module4/18-spring-boot-security-jwt/src/main/java/com/codegym/springbootsecurityjwt/configuration/SecurityInit.java
@@ -0,0 +1,6 @@
+package com.codegym.springbootsecurityjwt.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
